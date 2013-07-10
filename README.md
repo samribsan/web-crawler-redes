d07338fdcc6350aa65da483468c8aa4031ee6da9
ptkila <ptkila@inf.ufpel.edu.br>
2013-07-10 00:24:11 -0300

    Várias modificações, ler README.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 web-crawler-redes
 =====================
+
+Até agora:
+- Leitura e reconhecimento de links em pagínas
+- Profundidade já pode ser definida
+- Já é possível fazer o download do conteúdo dos links (não há garantia de se estar correto)
+
+Biliotecas:
+- urllib
+- urlparse
+- BeautifulSoup (não tenho certeza se pode usar)
--- web-crawler.py
+++ web-crawler.py
@@ -1,3 +1,32 @@
-import os
-a = raw_input("Digite um site! :")
-os.system("GET"+ " " + a)
+import urllib
+import urlparse
+import BeautifulSoup
+
+url = "http://www.ufpel.edu.br"
+nivelmax = 1
+
+urls = [url]
+visited = [url] 	
+
+while (nivelmax > 0):
+	try:
+		htmltext = urllib.urlopen(urls[0])
+	except:
+		print urls[0]
+	
+	soup = BeautifulSoup.BeautifulSoup(htmltext)
+
+	urls.pop(0)
+	print len(urls)
+
+	for tag in soup.findAll('a',href=True):
+		tag['href'] = urlparse.urljoin(url,tag['href'])
+		if url in tag['href'] and tag['href'] not in visited:
+			urls.append(tag['href'])
+			visited.append(str(tag['href']))
+
+	nivelmax = nivelmax - 1
+
+print visited
+for i in range (len(visited)):
+	urllib.urlretrieve(visited[i],visited[i].split('/')[-1])
